717a2d02ca011b0684fa1be14c5e9eee2124a8dd
Om GARG <101242375+Om0978@users.noreply.github.com>
2025-02-15 17:31:33 +0530

    feature : add new contact to address-book

--- addressBookProblem.cpp
+++ addressBookProblem.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 
-class contact{
-    public:
-
-    string firstName,lastName,address,city,state,email;
+class contact
+{
+public:
+    string firstName, lastName, address, city, state, email;
     int zip;
     long long phoneNumber;
-    
+
     // constructor for initialize variables
-    contact(string firstName,string lastName,string address,string city,string state,string email,int zip,long long phone){
+    contact(string firstName, string lastName, string address, string city, string state, string email, int zip, long long phone)
+    {
         this->firstName = firstName;
         this->lastName = lastName;
         this->address = address;
@@ -18,56 +19,86 @@ class contact{
         this->email = email;
         this->zip = zip;
         this->phoneNumber = phone;
-
     }
 
-    // function to display contact 
-    void displayContact(){
-        cout<<"\ncontact details : \n";
-        cout<<"name : "<<firstName<<" "<<lastName<<endl;
-        cout<<"address : "<<address<<", "<<city<<", "<<state<<"-"<<zip<<endl;
-        cout<<"phone : "<<phoneNumber<<endl;
-        cout<<"email : "<<email<<endl;
+    // function to display contact
+    void displayContact()
+    {
+        cout << "\ncontact details : \n";
+        cout << "name : " << firstName << " " << lastName << endl;
+        cout << "address : " << address << ", " << city << ", " << state << "-" << zip << endl;
+        cout << "phone : " << phoneNumber << endl;
+        cout << "email : " << email << endl;
     }
 };
 
-int main(){
-     
-    // welcome message 
-    cout<<"Welcome to Address Book Program\n";
+class addressBook
+{
+    vector<contact> contacts;
 
-    // contact details variables
-    string firstName,lastName,address,city,state,email;
-    int zip;
-    long long phoneNumber;
+public:
 
-    // take inputs from user
-    cout<<"enter first name \n";
-    cin>>firstName;
+// uc2
+    void addContact()
+    {
+        // contact details variables
+        string firstName, lastName, address, city, state, email;
+        int zip;
+        long long phoneNumber;
 
-    cout<<"enter last name \n";
-    cin>>lastName;
+        // take inputs from user
+        cout << "enter first name \n";
+        cin >> firstName;
 
-    cout<<"enter address \n";
-    cin.ignore();
-    getline(cin,address);
+        cout << "enter last name \n";
+        cin >> lastName;
 
-    cout<<"enter city \n";
-    cin>>city;
+        cout << "enter address \n";
+        cin.ignore();
+        getline(cin, address);
 
-    cout<<"enter state \n";
-    cin>>state;
+        cout << "enter city \n";
+        cin >> city;
 
-    cout<<"enter email \n";
-    cin>>email;
+        cout << "enter state \n";
+        cin >> state;
 
-    cout<<"enter zip code \n";
-    cin>>zip;
+        cout << "enter email \n";
+        cin >> email;
 
-    cout<<"enter phone number \n";
-    cin>>phoneNumber;
+        cout << "enter zip code \n";
+        cin >> zip;
 
-    contact p1(firstName,lastName,address,city,state,email,zip,phoneNumber);
+        cout << "enter phone number \n";
+        cin >> phoneNumber;
 
-    p1.displayContact();
+        contact newContact(firstName, lastName, address, city, state, email, zip, phoneNumber); // call contact class constructor to make new data
+        contacts.push_back(newContact); // add contact to vector
+    }
+
+    void display(){
+        if(contacts.empty()){
+            cout<<"\naddress book is empty !\n";
+        }
+        else{
+            for (auto i : contacts){
+
+                cout<<"-------------------------------\n";
+                i.displayContact();
+                cout<<"-------------------------------\n";
+            }
+        }
+    }
+};
+
+int main()
+{
+    // welcome message
+    cout << "Welcome to Address Book Program\n";
+
+    addressBook addressbook;
+    addressbook.addContact();
+    addressbook.display();
+
+    return 0;
 }
